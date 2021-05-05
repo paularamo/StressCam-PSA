537469300b6accf6aefb93eaa62fba1fe54ac14f
pjramg <pjramg@gmail.com>
2021-05-05 10:57:54 -0400

    ver 5.5.21

--- codes/ML_Corn/takeImages.py
+++ codes/ML_Corn/takeImages.py
@@ -38,8 +38,9 @@ treatment = device_params['Treatment']            #
 testML = device_params['test ML']
 mode = device_params['mode']
 time_zone = device_params['timezone']
-farm=device_params['farm']
-rep=device_params['rep']
+farm = device_params['farm']
+rep = device_params['rep']
+version = device_params['ver']
 ###################################################
 
 tz_splitted = time_zone.split("/")
@@ -59,6 +60,7 @@ timeStamp = curr.strftime("%d_%m_%Y_%H_%M_%S")
 file = '/home/pi/images/' + curr.strftime("%d_%m_%Y_%H_%M_%S") + '.jpg'
 
 camera.resolution=(2592,1944)
+camera.rotation = 180
 camera.capture(file)
 camera.close()
 
@@ -94,11 +96,11 @@ else:#read image just captured by camera
 ###SVM model for Corn, TFLite model for soybeans
 if (crop == "corn"):
     im = Image.fromarray(im,"RGB")
-    print("Resizing image...")
+    #print("Resizing image...")
     im_final = im.resize((324,216))#Model was trained on 324(w)x216(h) images
-    print("Captured and Resized Image!")
+    #print("Captured and Resized Image!")
     ########Convert RGB Image to LUX representation #############
-    print("Converting Image to LUX format...")
+    #print("Converting Image to LUX format...")
     img0 = np.asarray(im_final)
     LUX = np.zeros((img0.shape[0],img0.shape[1], 3),dtype=np.float32 )
     row = img0.shape[0]-1 #img0 = image we are manipulating. row = height of image. columns = width of image
@@ -122,22 +124,22 @@ if (crop == "corn"):
           LUX[i][j][0] = int(l)
           LUX[i][j][1] = int(u)
           LUX[i][j][2] = int(x)
-    print("Finished converting image to LUX!")
+    #print("Finished converting image to LUX!")
     ######Extract Histogram of Gradients from LUX Image ##########
-    print("Extracting HOG Features...")
+    #print("Extracting HOG Features...")
     pixels_per_cell = (9,9)
     cells_per_block = (24,36)
     fd = hog(LUX, orientations=9, pixels_per_cell=pixels_per_cell,cells_per_block=cells_per_block,  block_norm='L2', transform_sqrt=False, visualize= False, multichannel=True) #16,16
     ######Load the Model and make a prediction#############
-    print("Loading SVM model and making prediction...")
+    #print("Loading SVM model and making prediction...")
     svm_file = '/home/pi/ML_Corn/svm_tuned_modelFINAL.sav'
     loaded_model = pickle.load(open(svm_file, 'rb'))
     fd = fd.reshape(1,-1) #required if only one sample
     result = loaded_model.predict_proba(fd)
-    print("Evaulating Image: ", file)
-    print("Predicted Water Stress Probabilites:",result)
+    #print("Evaluating Image: ", file)
+    #print("Predicted Water Stress Probabilites:",result)
     waterStressLevel = int(np.argmax(result))+1
-    print("Predicted WS:", waterStressLevel)
+    #print("Predicted WS:", waterStressLevel)
     try:
         print("Actual WS: ", actual_label)
     except NameError:
@@ -153,7 +155,7 @@ else: #DL model for soybeans
     im_final=tf.keras.applications.mobilenet_v2.preprocess_input(im_final)
     # Load TFLite model and allocate tensors.
     im_final= np.array(im_final,dtype=np.float32)
-    print("allocating tensors")
+    #print("allocating tensors")
     interpreter = tflite.Interpreter(model_path="/home/pi/ML_Corn/soybeanTFLiteModel.tflite")
     interpreter.allocate_tensors()
     # Get input and output tensors.
@@ -164,17 +166,17 @@ else: #DL model for soybeans
     #test model
     input_data = np.expand_dims(Xtest,axis=0)
     interpreter.set_tensor(input_details[0]['index'], input_data)
-    print("invoking interpreter")
+    #print("invoking interpreter")
     interpreter.invoke()
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
     output_data = interpreter.get_tensor(output_details[0]['index'])
     results = np.squeeze(output_data)
-    print(results)
+    #print(results)
     waterStressLevel = int(np.argmax(results))
     percentConfident = results[waterStressLevel]*100
-    print("Predicted WS Level: ", waterStressLevel)
-    print("Confidence: ",percentConfident)
+    #print("Predicted WS Level: ", waterStressLevel)
+    #print("Confidence: ",percentConfident)
     try:
         print("Actual WS: ", actual_label)
     except NameError:
@@ -187,17 +189,20 @@ if (crop =="corn"):
                     "CPU_TEMP": cpu_temp,
                     "DATE":currDate,
                     "TIME":currTime,
-                    "SD_free":disk_free,
+                    "SD_free":round(disk_free,2),
+                    "FARM":farm,
+                    "REP":rep,
                     "CROP": crop,
-                    "TREATMENT": treatment,
+                    "TRT": treatment,
                     "MODE": mode,
                     "ZONE": tz_splitted[1],
                     "FILE":street[randomImage],
-                    "P_WS_1": round(result[0][0],5),
-                    "P_WS_2": round(result[0][1],5),
-                    "P_WS_3": round(result[0][2],5),
+                    "P_WS_1": round(result[0][0],4),
+                    "P_WS_2": round(result[0][1],4),
+                    "P_WS_3": round(result[0][2],4),
                     "P_WS":waterStressLevel,
                     "A_WS": actual_label,
+                    "VER": version,
                 }
     else:
         data = {
@@ -206,17 +211,19 @@ if (crop =="corn"):
                     "CPU_TEMP": cpu_temp,
                     "DATE":currDate,
                     "TIME":currTime,
-                    "SD_free":disk_free,
+                    "SD_free":round(disk_free,2),
+                    "FARM":farm,
+                    "REP":rep,
                     "CROP": crop,
-                    "TREATMENT": treatment,
+                    "TRT": treatment,
                     "MODE": mode,
                     "ZONE": tz_splitted[1],
                     "FILE":timeStamp,
-                    "P_WS_1": round(result[0][0],5),
-                    "P_WS_2": round(result[0][1],5),
-                    "P_WS_3": round(result[0][2],5),
+                    "P_WS_1": round(result[0][0],4),
+                    "P_WS_2": round(result[0][1],4),
+                    "P_WS_3": round(result[0][2],4),
                     "P_WS":waterStressLevel,
-
+                    "VER": version,
                 }
 else:#different payloads for Soybean cams
     if testML=="true":
@@ -226,20 +233,23 @@ else:#different payloads for Soybean cams
                     "CPU_TEMP": cpu_temp,
                     "DATE":currDate,
                     "TIME":currTime,
-                    "SD_free":disk_free,
+                    "SD_free":round(disk_free,2),
+                    "FARM":farm,
+                    "REP":rep,
                     "CROP": crop,
-                    "TREATMENT": treatment,
+                    "TRT": treatment,
                     "MODE": mode,
                     "ZONE": tz_splitted[1],
                     "FILE":"Test Image",
-                    "P_WS_0": round(float(results[0]),5),
-                    "P_WS_1": round(float(results[1]),5),
-                    "P_WS_2": round(float(results[2]),5),
-                    "P_WS_3": round(float(results[3]),5),
-                    "P_WS_4": round(float(results[4]),5),
-                    "P_WS_5": round(float(results[5]),5),
+                    "P_WS_0": round(float(results[0]),4),
+                    "P_WS_1": round(float(results[1]),4),
+                    "P_WS_2": round(float(results[2]),4),
+                    "P_WS_3": round(float(results[3]),4),
+                    "P_WS_4": round(float(results[4]),4),
+                    "P_WS_5": round(float(results[5]),4),
                     "P_WS":waterStressLevel,
                     "A_WS": int(actual_label),
+                    "VER": version,
                 }
     else:
         data = {
@@ -248,22 +258,30 @@ else:#different payloads for Soybean cams
                     "CPU_TEMP": cpu_temp,
                     "DATE":currDate,
                     "TIME":currTime,
-                    "SD_free":disk_free,
+                    "SD_free":round(disk_free,2),
+                    "FARM":farm,
+                    "REP":rep,
                     "CROP": crop,
-                    "TREATMENT": treatment,
+                    "TRT": treatment,
                     "MODE": mode,
                     "ZONE": tz_splitted[1],
                     "FILE":timeStamp,
-                    "P_WS_0": round(float(results[0]),5),
-                    "P_WS_1": round(float(results[1]),5),
-                    "P_WS_2": round(float(results[2]),5),
-                    "P_WS_3": round(float(results[3]),5),
-                    "P_WS_4": round(float(results[4]),5),
-                    "P_WS_5": round(float(results[5]),5),
+                    "P_WS_0": round(float(results[0]),4),
+                    "P_WS_1": round(float(results[1]),4),
+                    "P_WS_2": round(float(results[2]),4),
+                    "P_WS_3": round(float(results[3]),4),
+                    "P_WS_4": round(float(results[4]),4),
+                    "P_WS_5": round(float(results[5]),4),
                     "P_WS":waterStressLevel,
+                    "VER": version,
                 }
-print(data)
+#print(data)
 recv = hologram.sendMessage(data) # Send message to hologram cloud
 print("Recieved Code:",recv)
 print("0 Means Succesful Transmission")
+
+with open('data.txt', 'a') as outfile:
+            json.dump(data, outfile)
+            outfile.write('\n')
+
 sleep(20)
